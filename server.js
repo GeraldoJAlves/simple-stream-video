e32a07a6d636200a0d4ac3d7b930b50f61fc266a
GeraldoJAlves <junior.alves.2007@gmail.com>
2020-11-17 04:17:57 -0300

    streaming vs no streaming

--- server.js
+++ server.js
@@ -3,35 +3,69 @@ const express = require("express");
 
 const app = express();
 
+const requestFile = (req, res, pathFile) => {
+  if (!fs.existsSync(pathFile)) {
+    return res.status(404).end("<h1>File not found</h1>");
+  }
+  fs.readFile(pathFile, (err, content) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).end("internal error");
+    }
+    res.end(content);
+  });
+}
+
+app.get("/:file", (req, res) => {
+  let file = req.params.file;
+  const pathFile = `./public/${file}`;
+  requestFile(req, res, pathFile)
+});
+
 app.get("/", (req, res) => {
-  fs.readFile("./public/index.html", (err, content) => res.end(content));
+  let file = "index.html"
+  const pathFile = `./public/${file}`;
+  requestFile(req, res, pathFile)
 });
 
 app.get("/movies/:movieName", (req, res) => {
   const { movieName } = req.params;
   const movieFile = `./movies/${movieName}`;
-  fs.stat(movieFile, (err, stats) => {
-    if (err) {
-      return res.status(404).end("<h1>Movie not found</h1>");
-    }
-    const { range } = req.headers;
-    const { size } = stats;
-    const parts = (range || "").replace(/bytes=/, "").split("-")
-    const start = Number(parts[0]);
-    const end = size - 1;
-    const chunkSize = end - start + 1;
-    // console.log(chunkSize, start, end)
-    console.log(parts)
-    res.set({
-      "Content-Range": `bytes ${start}-${end}/${size}`,
+  requestFile(req, res, movieFile)
+});
+
+app.get("/stream/:movieName", (req, res) => {
+  const { movieName } = req.params;
+  const movieFile = `./movies/${movieName}`;
+
+  if (!fs.existsSync(movieFile)) {
+    return res.status(404).end("<h1>Movie not found</h1>");
+  }
+  const stat = fs.statSync(movieFile);
+  const fileSize = stat.size;
+  const { range } = req.headers;
+  if (range) {
+    const parts = range.replace(/bytes=/, "").split("-");
+    const start = parseInt(parts[0], 10);
+    const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
+    const chunksize = end - start + 1;
+    const file = fs.createReadStream(movieFile, { start, end });
+    const head = {
+      "Content-Range": `bytes ${start}-${end}/${fileSize}`,
       "Accept-Ranges": "bytes",
-      "Content-Length": chunkSize,
+      "Content-Length": chunksize,
       "Content-Type": "video/mp4",
-    });
-    const stream = fs.createReadStream(movieFile, { start, end });
-    stream.on("open", () => stream.pipe(res));
-    stream.on("error", (streamErr) => res.end(streamErr));
-  });
+    };
+    res.writeHead(206, head);
+    file.pipe(res);
+  } else {
+    const head = {
+      "Content-Length": fileSize,
+      "Content-Type": "video/mp4",
+    };
+    res.writeHead(200, head);
+    fs.createReadStream(movieFile).pipe(res);
+  }
 });
 
 app.listen(3000, () => {
